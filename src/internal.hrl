f8c126f6d35bb209745443ca46ee3196444a253d
Eric Merritt <eric.merritt@five9.com>
2012-10-21 11:45:42 -0500

    rename erlcron-internal to internal.hrl
    Signed-off-by: Jordan Wilberding <diginux@gmail.com>

---
+++ src/internal.hrl
@@ -0,0 +1,6 @@
+%%% @copyright Erlware, LLC. All Rights Reserved.
+%%%
+%%% This file is provided to you under the BSD License; you may not use
+%%% this file except in compliance with the License.  You may obtain a
+%%% copy of the License.
+-define(ONE_DAY, (24 * 60 * 60)).
