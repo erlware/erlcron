194110ac6cfdc69431d1373affa7456269e4d2d0
Srijan Choudhary <srijan4@gmail.com>
2016-07-11 15:09:58 +0530

    Fix specs for OTP 19

--- src/ecrn_cron_sup.erl
+++ src/ecrn_cron_sup.erl
@@ -22,14 +22,14 @@
 %%% API functions
 %%%===================================================================
 
--spec start_link/0 :: () -> {ok, pid()} | ignore | {error, Error::term()}.
+-spec start_link() -> {ok, pid()} | ignore | {error, Error::term()}.
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 
 %% @doc
 %%  Add a chron job to be supervised
--spec add_job/2 :: (erlcron:job_ref(), erlcron:job()) -> erlcron:job_ref().
+-spec add_job(erlcron:job_ref(), erlcron:job()) -> erlcron:job_ref().
 add_job(JobRef, Task) ->
     {ok, _} = supervisor:start_child(?SERVER, [JobRef, Task]),
     JobRef.
